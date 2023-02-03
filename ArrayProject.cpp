ca78bff914153d60997440bcae5b901def0e645b
Radmila Makieva <makievaradmila@mail.ru>
2023-02-03 21:29:50 +0300

    We started work with arrays

--- ArrayProject.cpp
+++ ArrayProject.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "");
-    cout << "Hello World!\n";
+    short mas[10] = {};
+    for (int i = 0; i <= 9; i++) {
+        mas[i] = i+1;
+        cout << mas[i] << endl;
+    }
+
 }
 
