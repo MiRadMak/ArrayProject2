f4614e18e4d09e46d639b01cb6d1c1a524236e65
Radmila Makieva <makievaradmila@mail.ru>
2023-02-08 21:30:02 +0300

    Find min, max, average and zero

--- ArrayProject.cpp
+++ ArrayProject.cpp
@@ -4,11 +4,41 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "");
-    short mas[10] = {};
-    for (int i = 0; i <= 9; i++) {
-        mas[i] = i+1;
-        cout << mas[i] << endl;
+    const int SIZE = 20;
+    const int MAX_RAND_VALUE = 1000;
+    short mas[SIZE] = {};
+    int i;
+    int min = MAX_RAND_VALUE + 1;
+    int max = 0;
+    int srednee = 0;
+    int n = 0;
+    
+    for (i = 0; i < SIZE; i++) {
+        mas[i] = rand() % MAX_RAND_VALUE;
+        if (mas[i] <  min) {
+            min = mas[i];
+        }
+        if (mas[i] > max) {
+            max = mas[i];
+        }
+        
     }
+    srednee = n / SIZE;
+    cout << "Среднее - " << srednee << endl;
+    cout << "MIN - " << min << endl;
+    cout << "MAX - " << max << endl;
+    for (int i = 0; i < SIZE; i++) {
+        cout << mas[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < SIZE; i++) {
+        n += mas[i];
+        if (mas[i] % 2 != 0) {
+            mas[i] = 0;     
+        }cout << mas[i] << " ";
+    }
+
+
 
 }
 
